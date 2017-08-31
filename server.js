5b8f0a5565f864141136e6f1d08bb1c24d2768ca
Mackoyokcam <mark.hangarter@gmail.com>
2017-08-31 00:09:36 -0700

    first attempt at creating heroku

--- server.js
+++ server.js
@@ -3,9 +3,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const requestProxy = require('express-request-proxy');
 
 // DONE: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
+
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+
+app.get('/github/*', proxyGitHub);
 
 app.listen(PORT, function() {
   // DONE: Log to the console a message that lets you know which port your server has started on
